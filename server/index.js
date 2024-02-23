4f78b433e0dd08a73532fe3f678a5c4ec76cc035
habib-SE <hab67603@gmail.com>
2024-02-23 23:17:30 +0500

    update the code

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-import express from "express"
-import dotenv from "dotenv"
 
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { User } from './models/userModel.js';
 dotenv.config();
-
+const myusername= process.env.USER;
+const password= process.env.PASSWORD;
 const app = express();
+mongoose.connect(`mongodb+srv://${myusername}:${password}@authentication.ib4cnrl.mongodb.net/?retryWrites=true&w=majority&appName=Authentication`);
 
-app.listen(process.env.PORT, () =>{
-    console.log("server is running")
-})
+app.listen(process.env.PORT, () => {
+    console.log(`server is running ${process.env.PORT}`);
+});
